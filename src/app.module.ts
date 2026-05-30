1a8c5455254bdfa42e1d0ae77d249deb1a96b6ef
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2026-05-30 09:01:31 -0300

    refactor: only specify necessary objects in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,12 @@ import config from './config';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
-import { AuthService } from './auth/auth.service';
 import { QuoteModule } from './quote/quote.module';
-import { AuthController } from './auth/auth.controller';
 import { DatabaseService } from './database/database.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, load: [config] }), AuthModule, QuoteModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService, DatabaseService],
+  controllers: [AppController],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
