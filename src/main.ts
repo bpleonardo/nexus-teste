359cf1eaf19cbae0e50308982d9077ef8b12c8be
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2026-05-30 10:53:01 -0300

    feat: use configservice for port

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import cookieparser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   app.use(cookieparser());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get('app.port') ?? 3000);
 }
 
 bootstrap();
